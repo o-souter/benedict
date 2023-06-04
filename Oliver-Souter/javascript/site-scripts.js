dda1adc595b4ac35d7a66bb3d4d1b39cc36768c4
o-souter <o.souter@rgu.ac.uk>
2023-06-04 17:50:03 +0100

    almost finished dark mode

--- Oliver-Souter/javascript/site-scripts.js
+++ Oliver-Souter/javascript/site-scripts.js
@@ -10,8 +10,23 @@ const introTitle = document.getElementById('intro-title');
 const intro = document.getElementById('intro');
 const moreAboutMeTitle = document.getElementById('more-about-me-title');
 const moreAboutMeTxt = document.getElementById('more-about-me-div')
+const moreAboutMe = document.getElementById('more-about-me')
+const hyperlinks = document.getElementsByTagName('a')
+const skillsTitle = document.getElementById('skills-title')
+const skillCells = document.getElementsByClassName('skill-cell');
+const projects = document.getElementById('projects');
+const projectsTitle = document.getElementById('projects-title');
+const languagesTitle = document.getElementById('languages-title');
+const projectCells = document.getElementsByClassName('project-cell');
+const projectTitles = document.getElementsByClassName('project-title');
+const projectDescriptions = document.getElementsByClassName('project-description')
+const contactMeTitle = document.getElementById('contactme-title')
+const contactMe = document.getElementById('contact-form-div')
+const contactTxt = document.getElementsByClassName('contact-text')
+const contactInputs = document.getElementsByClassName('inputs');
+const findme = document.getElementById('findme')
+const findmeCells = document.getElementsByClassName("findme-cell")
 
-//html.dataset.theme = light;
 //Hamburger menu scripts
 
 const hamburgermenu = document.getElementById("hamburger-menu");
@@ -40,12 +55,36 @@ function toggleLightDarkMode() {
         sunMoon.classList.add("fa-regular");
         sunMoon.classList.remove("fa-solid");
         html.style.backgroundColor = "#0e0833";
-        lightDarkToggle.innerHTML
+        for (const el of hyperlinks) {
+            if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#00ff22"}
+        }
+
+        //hyperlinks.forEach().style.color = "#00ff22"
         introText.style.color = "white";
         introTitle.style.color = "white";
         intro.style.boxShadow = "none";
+        moreAboutMe.style.backgroundColor = "#464682";
         moreAboutMeTitle.style.color = "white";
         moreAboutMeTxt.style.color = "white";
+        skillsTitle.style.color = "white";
+        for (const el of skillCells) {el.style.color = "white"}
+        languagesTitle.style.color = "white"
+        projects.style.backgroundColor = "#464682"
+        projectsTitle.style.color = "white"
+        for (const el of projectCells) {el.style.backgroundColor = "#545252"}
+        for (const el of projectTitles) {el.style.color = "white"}
+        for (const el of projectDescriptions) {el.style.color = "white"}
+        contactMeTitle.style.color = "white"
+        contactMe.style.boxShadow = "none"
+        for (const el of contactTxt) {el.style.color = "white"}
+        for (const el of contactInputs) {
+            el.style.border = "5px solid white"
+            el.style.backgroundColor = "#0e0833"
+            el.style.color = "white"
+        }
+        findme.style.backgroundColor = "#464682"
+        findme.style.color = "white"
+        for (const el of findmeCells) {el.style.backgroundColor = "#545252"}
     }
     
     else if (mode == 'dark') {
@@ -53,11 +92,35 @@ function toggleLightDarkMode() {
         sunMoon.classList.remove("fa-regular");
         sunMoon.classList.add("fa-solid");
         html.style.backgroundColor = "white";
+        for (const el of hyperlinks) {
+            if (el.className != 'nav-link' && el.id != 'hamburger-menu') {el.style.color = "#007910"}
+        }
         introText.style.color = "black";
         introTitle.style.color = "black";
         intro.style.boxShadow = "0px 2px 15px 2px var(--primary-shadow)"
+        moreAboutMe.style.backgroundColor = "#9bd8e9";
         moreAboutMeTitle.style.color= "black"
         moreAboutMeTxt.style.color = "black";
+        skillsTitle.style.color = "black";
+        for (const el of skillCells) {el.style.color = "black"}
+        languagesTitle.style.color = "black"
+        projects.style.backgroundColor = "#9bd8e9"
+        projectsTitle.style.color = "black"
+        for (const el of projectCells) {el.style.backgroundColor = "#ebebeb"}
+        for (const el of projectTitles) {el.style.color = "black"}
+        for (const el of projectDescriptions) {el.style.color = "black"}
+        contactMeTitle.style.color = "black"
+        contactMe.style.boxShadow = "0 3px 10px var(--secondary-shadow)"
+        for (const el of contactTxt) {el.style.color = "black"}
+        for (const el of contactInputs) {
+            el.style.border = "5px solid black"
+            el.style.backgroundColor = "white"
+            el.style.color = "black"
+        }
+        findme.style.backgroundColor = "#9bd8e9"
+        findme.style.color = "black"
+        for (const el of findmeCells) {el.style.backgroundColor = "#ebebeb"}
+
     }
 
     
